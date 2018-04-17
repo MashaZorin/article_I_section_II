89b8366d1c2fa265171b19bed757f714be4f19c9
Kyle Lin <klin12@stuy.edu>
2018-04-17 08:39:55 -0400

    Fixed loading

--- static/js/map.js
+++ static/js/map.js
@@ -2,21 +2,20 @@ var svg = d3.select("svg");
 var result;
 var path = d3.geoPath();
 var cityLat, cityLng;
-d3.json("usmap.json", function (error, us) {
+
+d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
   if (error) throw error;
 
   svg.append("g")
-    .attr("class", "states")
+      .attr("class", "states")
     .selectAll("path")
     .data(topojson.feature(us, us.objects.states).features)
     .enter().append("path")
-    .attr("d", path);
+      .attr("d", path);
 
   svg.append("path")
-    .attr("class", "state-borders")
-    .attr("d", path(topojson.mesh(us, us.objects.states, function (a, b) {
-      return a !== b;
-    })));
+      .attr("class", "state-borders")
+      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
   
   var d = [
     [500, 500]
