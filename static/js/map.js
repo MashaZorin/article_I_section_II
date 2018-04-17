7b4cc4c474b29daaa4362c150ae726311d93f47f
Kyle Lin <klin12@stuy.edu>
2018-04-17 12:37:07 -0400

    Added population data

--- static/js/map.js
+++ static/js/map.js
@@ -3,24 +3,26 @@ var result;
 var path = d3.geoPath();
 var cityLat, cityLng;
 
-d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+d3.json("https://d3js.org/us-10m.v1.json", function (error, us) {
   if (error) throw error;
 
   svg.append("g")
-      .attr("class", "states")
+    .attr("class", "states")
     .selectAll("path")
     .data(topojson.feature(us, us.objects.states).features)
     .enter().append("path")
-      .attr("d", path);
+    .attr("d", path);
 
   svg.append("path")
-      .attr("class", "state-borders")
-      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
-  
+    .attr("class", "state-borders")
+    .attr("d", path(topojson.mesh(us, us.objects.states, function (a, b) {
+      return a !== b;
+    })));
+
   var d = [
     [500, 500]
   ];
-  
+
   var g = function (n) {
     var svgc = svg.selectAll("circle")
       .data(d)
@@ -33,30 +35,32 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
         return d[1];
       })
       .attr("r", 20)
-      .on("mouseover", function(d) {      
-        d3.select(this).transition().duration(250).style("fill", "#00aeef"); 
-    })
-      .on("mouseout", function(d) {
+      .on("mouseover", function (d) {
+        d3.select(this).transition().duration(250).style("fill", "#00aeef");
+      })
+      .on("mouseout", function (d) {
         d3.select(this).transition().duration(500).style("fill", "black");
-      }) ;
+      });
   };
-  
+
   g(0);
 });
 
-function getCoordinates(){
+function getCoordinates() {
 
-    var geocoder = new google.maps.Geocoder();
-    var address = "New York City";
-    geocoder.geocode({'address': address}, function(results, status) {
-        if (status === 'OK') {
-            result = results[0].geometry.location;
-	    cityLat = result.lat();
-	    cityLng = result.lng();
+  var geocoder = new google.maps.Geocoder();
+  var address = "New York City";
+  geocoder.geocode({
+    'address': address
+  }, function (results, status) {
+    if (status === 'OK') {
+      result = results[0].geometry.location;
+      cityLat = result.lat();
+      cityLng = result.lng();
       console.log(cityLat);
       console.log(cityLng);
-        } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-        }
-    });
-}
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+};
